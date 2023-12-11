3935ca966c018a4630435e6d3008de336d551b53
Nattot <danlexder@gmail.com>
2023-12-11 11:49:32 +0700

    IngfoKelas

---
+++ src/Component/ClassInfoCard.jsx
@@ -0,0 +1,19 @@
+const CardPart = ({ data }) => (
+  <div className="card-container">
+    {data.map((item, index) => (
+      <div key={index} className="card">
+        <div className="class-code pb-1">
+          Class <span style={{ marginLeft: '30px' }}>{item.class}</span>
+        </div>
+        <div className="date-topic pb-1">
+          Date <span style={{ marginLeft: '62px' }}>{item.topicsAndDates[0].date}</span>
+        </div>
+        <div className="date-topic pb-2">
+          Topic <span style={{ marginLeft: '54px', fontWeight: '700' }}>{item.topicsAndDates[0].topic}</span>
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
+export default CardPart;
