b258e73488cccc3d055c3e52a1083f873f40e3be
Nattot <danlexder@gmail.com>
2023-12-18 10:29:07 +0700

    up info kelas

--- src/Component/ClassInfoCard.jsx
+++ src/Component/ClassInfoCard.jsx
@@ -5,12 +5,16 @@ const CardPart = ({ data }) => (
         <div className="class-code pb-1">
           Class <span style={{ marginLeft: '30px' }}>{item.class}</span>
         </div>
-        <div className="date-topic pb-1">
-          Date <span style={{ marginLeft: '62px' }}>{item.topicsAndDates[0].date}</span>
-        </div>
-        <div className="date-topic pb-2">
-          Topic <span style={{ marginLeft: '54px', fontWeight: '700' }}>{item.topicsAndDates[0].topic}</span>
-        </div>
+        {item.topicsAndDates && Array.isArray(item.topicsAndDates) && item.topicsAndDates.length > 0 && (
+          <>
+            <div className="date-topic pb-1">
+              Date <span style={{ marginLeft: '62px' }}>{item.topicsAndDates[0].date}</span>
+            </div>
+            <div className="date-topic pb-2">
+              Topic <span style={{ marginLeft: '54px', fontWeight: '700' }}>{item.topicsAndDates[0].topic}</span>
+            </div>
+          </>
+        )}
       </div>
     ))}
   </div>
