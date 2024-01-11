68f7f0089491150f6fff0085fef11ea944058d91
Bryan <bryansupriadi717@gmail.com>
2024-01-11 13:14:12 +0700

    student data temp

---
+++ src/studentData.js
@@ -0,0 +1,212 @@
+export const studentData = [
+	{
+		"id": 1,
+		"studentName": "Wesley",
+		"task": 97,
+		"preTest": 75,
+		"postTest": 63
+	},
+	{
+		"id": 2,
+		"studentName": "Mechelle",
+		"task": 69,
+		"preTest": 98,
+		"postTest": 58
+	},
+	{
+		"id": 3,
+		"studentName": "Skyler",
+		"task": 78,
+		"preTest": 56,
+		"postTest": 75
+	},
+	{
+		"id": 4,
+		"studentName": "Kirsten",
+		"task": 78,
+		"preTest": 51,
+		"postTest": 54
+	},
+	{
+		"id": 5,
+		"studentName": "Caryn",
+		"task": 72,
+		"preTest": 50,
+		"postTest": 53
+	},
+	{
+		"id": 6,
+		"studentName": "Ulysses",
+		"task": 91,
+		"preTest": 55,
+		"postTest": 51
+	},
+	{
+		"id": 7,
+		"studentName": "Noah",
+		"task": 52,
+		"preTest": 90,
+		"postTest": 78
+	},
+	{
+		"id": 8,
+		"studentName": "Hakeem",
+		"task": 80,
+		"preTest": 72,
+		"postTest": 55
+	},
+	{
+		"id": 9,
+		"studentName": "Gwendolyn",
+		"task": 70,
+		"preTest": 59,
+		"postTest": 51
+	},
+	{
+		"id": 10,
+		"studentName": "Delilah",
+		"task": 57,
+		"preTest": 53,
+		"postTest": 54
+	},
+	{
+		"id": 11,
+		"studentName": "Desirae",
+		"task": 70,
+		"preTest": 58,
+		"postTest": 53
+	},
+	{
+		"id": 12,
+		"studentName": "Jasmine",
+		"task": 73,
+		"preTest": 88,
+		"postTest": 77
+	},
+	{
+		"id": 13,
+		"studentName": "Clinton",
+		"task": 99,
+		"preTest": 87,
+		"postTest": 51
+	},
+	{
+		"id": 14,
+		"studentName": "Sharon",
+		"task": 51,
+		"preTest": 84,
+		"postTest": 58
+	},
+	{
+		"id": 15,
+		"studentName": "Castor",
+		"task": 78,
+		"preTest": 67,
+		"postTest": 52
+	},
+	{
+		"id": 16,
+		"studentName": "Chandler",
+		"task": 97,
+		"preTest": 92,
+		"postTest": 61
+	},
+	{
+		"id": 17,
+		"studentName": "Mira",
+		"task": 98,
+		"preTest": 50,
+		"postTest": 80
+	},
+	{
+		"id": 18,
+		"studentName": "Addison",
+		"task": 84,
+		"preTest": 61,
+		"postTest": 90
+	},
+	{
+		"id": 19,
+		"studentName": "Martha",
+		"task": 87,
+		"preTest": 69,
+		"postTest": 96
+	},
+	{
+		"id": 20,
+		"studentName": "Amity",
+		"task": 64,
+		"preTest": 99,
+		"postTest": 63
+	},
+	{
+		"id": 21,
+		"studentName": "Yen",
+		"task": 68,
+		"preTest": 79,
+		"postTest": 97
+	},
+	{
+		"id": 22,
+		"studentName": "April",
+		"task": 90,
+		"preTest": 93,
+		"postTest": 92
+	},
+	{
+		"id": 23,
+		"studentName": "Mia",
+		"task": 84,
+		"preTest": 79,
+		"postTest": 67
+	},
+	{
+		"id": 24,
+		"studentName": "Anika",
+		"task": 89,
+		"preTest": 69,
+		"postTest": 99
+	},
+	{
+		"id": 25,
+		"studentName": "Beck",
+		"task": 73,
+		"preTest": 75,
+		"postTest": 53
+	},
+	{
+		"id": 26,
+		"studentName": "Kibo",
+		"task": 83,
+		"preTest": 75,
+		"postTest": 60
+	},
+	{
+		"id": 27,
+		"studentName": "Harlan",
+		"task": 70,
+		"preTest": 84,
+		"postTest": 75
+	},
+	{
+		"id": 28,
+		"studentName": "Elvis",
+		"task": 86,
+		"preTest": 87,
+		"postTest": 75
+	},
+	{
+		"id": 29,
+		"studentName": "Dana",
+		"task": 99,
+		"preTest": 66,
+		"postTest": 84
+	},
+	{
+		"id": 30,
+		"studentName": "Francis",
+		"task": 71,
+		"preTest": 99,
+		"postTest": 59
+	}
+]
