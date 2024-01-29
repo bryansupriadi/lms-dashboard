0d0a5a741bfe35e404f70dc2c9b95bc98029ea49
Bryan <bryansupriadi717@gmail.com>
2024-01-29 15:46:00 +0700

    task page

--- src/Component/Task.jsx
+++ src/Component/Task.jsx
@@ -1,9 +1,157 @@
-function Task() {
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { studentData } from '../studentData';
+import '../style/ClassDetail.css';
 
-    return(
+function Task() {
+    const { topic, classCode } = useParams();
+    const pageSize = 5;
+
+    function getRandomProgress() {
+        return `${Math.floor(Math.random() * 11) * 10}%`;
+    }
+
+    function convertToTitleCase(str) {
+        // Ganti semua karakter '-' dengan spasi
+        str = str.replace(/-/g, " ");
+      
+        // Ganti semua kata pertama dari setiap kata dengan huruf kapital
+        return str.replace(/\b\w/g, (match) => match.toUpperCase());
+    }
+
+    const filteredStudents = studentData
+        .filter(student => student.classCode.toLowerCase() === classCode.toLowerCase())
+        .map(student => ({
+            ...student,
+            progress: getRandomProgress(),
+        }));
+
+    const [currentPage, setCurrentPage] = useState(0);
+    const [activeButton, setActiveButton] = useState(null);
+    const [isPaginationActive, setPaginationActive] = useState(false);
+    const [isPopupOpen, setPopupOpen] = useState(false);
+    const [selectedStudent, setSelectedStudent] = useState(null);
+    const [nilai, setNilai] = useState('');
+    const [error, setError] = useState('');
+
+    const totalPages = Math.ceil(filteredStudents.length / pageSize);
+
+    const handlePageChange = newPage => {
+        setPaginationActive(true);
+        setActiveButton(newPage);
+        setTimeout(() => {
+            setCurrentPage(newPage);
+            setPaginationActive(false);
+        }, 500); // Set timeout to simulate overlay effect
+    };
+
+    const handleNilaiChange = event => {
+        const inputValue = event.target.value;
+        // Validasi apakah nilai yang dimasukkan adalah angka dan berada dalam rentang 1-100
+        if (!(/^\d+$/.test(inputValue)) || inputValue < 1 || inputValue > 100) {
+            setError('Nilai harus berupa angka 1-100');
+        } else {
+            setError('');
+        }
+        setNilai(inputValue);
+    };
+
+    const handleSubmitNilai = () => {
+        // Lakukan sesuatu dengan nilai yang diinputkan, misalnya menyimpannya
+        // Reset nilai dan tutup popup setelah submit
+        setNilai('');
+        setPopupOpen(false);
+    };
+
+    const generatePageNumbers = () => {
+        const pageNumbers = [];
+        for (let i = 0; i < totalPages; i++) {
+            pageNumbers.push(
+                <button
+                    key={i}
+                    onClick={() => handlePageChange(i)}
+                    className={`${currentPage === i ? 'active' : ''} ${activeButton === i ? 'pressed' : ''} ${
+                        isPaginationActive ? 'overlay' : ''
+                    }`}
+                >
+                    {i + 1}
+                </button>
+            );
+        }
+        return pageNumbers;
+    };
+
+    const openPopup = student => {
+        setSelectedStudent(student);
+        setPopupOpen(true);
+    };
+    
+    const closePopup = () => {
+        setSelectedStudent(null);
+        setNilai('');
+        setError('');
+        setPopupOpen(false);
+    };
+
+    return (
         <div className="task-container">
+            <table className="task-table">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Terakhir Kumpul</th>
+                        <th>Nilai</th>
+                        <th>Detail</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredStudents
+                        .slice(currentPage * pageSize, (currentPage + 1) * pageSize)
+                        .map((student, index) => (
+                            <tr key={currentPage * pageSize + index + 1}>
+                                <td>{currentPage * pageSize + index + 1}</td>
+                                <td>{student.studentName}</td>
+                                <td>{student.lastUpdate}</td>
+                                <td>{student.task}</td>
+                                <td>
+                                    <button onClick={() => openPopup(student)} style={{border: 'none', backgroundColor: '#fff', color: '#2D76E5'}}>Lihat Tugas</button>
+                                </td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+            <div className="pagination">{generatePageNumbers()}</div>
+
+            {isPopupOpen && (
+                <div className="popup">
+                    <div className="popup-content">
+                        <span className="close" onClick={closePopup}>
+                            &times;
+                        </span>
+                        <h1 className='fs-4' style={{fontWeight: 'bold'}}>Tugas Mandiri</h1>
+                        {/* Tampilkan detail tugas di sini */}
+                        <h5 className='fs-5' style={{fontWeight: '400'}}>Topik: {convertToTitleCase(topic)}</h5>
+                        <p className='pt-3'>Jawaban:</p>
+                        <div className='d-flex align-items-center justify-content-center pb-2'>
+                            <img src={selectedStudent?.icon} alt="tugas"/>
+                        </div>
+                        <p>Ini hasil jawaban saya, silahkan diperiksa. Terima kasih</p>
+                        
+                        {/* Form untuk memberi nilai */}
+                        <form onSubmit={(e) => { e.preventDefault(); handleSubmitNilai(); }}>
+                            <label>
+                                Beri nilai:
+                                <input type="text" value={nilai} onChange={handleNilaiChange} style={{marginLeft: '10px'}}/>
+                            </label>
+                            {error && <p className="error">{error}</p>}
+                            {nilai !== '' && !error && <button type="submit" style={{backgroundColor: '#2D76E5', color: 'white', border: 'none', borderRadius: '10px', width: '15vh', height: '5vh', marginLeft: '10px'}}>Submit</button>}
+                        </form>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
 export default Task;
--- src/studentData.js
+++ src/studentData.js
@@ -1,242 +1,304 @@
+import taskIcon from './Assets/Rectangle 4308.png';
+
 export const studentData = [
 	{
 		"id": 1,
 		"studentName": "Wesley",
 		"classCode": "LB-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 97,
 		"preTest": 75,
-		"postTest": 63
+		"postTest": 63,
+		"icon": taskIcon,
 	},
 	{
 		"id": 2,
 		"studentName": "Mechelle",
 		"classCode": "LC-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 69,
 		"preTest": 98,
-		"postTest": 58
+		"postTest": 58,
+		"icon": taskIcon
 	},
 	{
 		"id": 3,
 		"studentName": "Skyler",
 		"classCode": "LB-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 78,
 		"preTest": 56,
-		"postTest": 75
+		"postTest": 75,
+		"icon": taskIcon
 	},
 	{
 		"id": 4,
 		"studentName": "Kirsten",
 		"classCode": "LD-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 78,
 		"preTest": 51,
-		"postTest": 54
+		"postTest": 54,
+		"icon": taskIcon
 	},
 	{
 		"id": 5,
 		"studentName": "Caryn",
 		"classCode": "LC-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 72,
 		"preTest": 50,
-		"postTest": 53
+		"postTest": 53,
+		"icon": taskIcon
 	},
 	{
 		"id": 6,
 		"studentName": "Ulysses",
 		"classCode": "LC-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 91,
 		"preTest": 55,
-		"postTest": 51
+		"postTest": 51,
+		"icon": taskIcon
 	},
 	{
 		"id": 7,
 		"studentName": "Noah",
 		"classCode": "LB-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 52,
 		"preTest": 90,
-		"postTest": 78
+		"postTest": 78,
+		"icon": taskIcon
 	},
 	{
 		"id": 8,
 		"studentName": "Hakeem",
 		"classCode": "LD-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 80,
 		"preTest": 72,
-		"postTest": 55
+		"postTest": 55,
+		"icon": taskIcon
 	},
 	{
 		"id": 9,
 		"studentName": "Gwendolyn",
 		"classCode": "LC-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 70,
 		"preTest": 59,
-		"postTest": 51
+		"postTest": 51,
+		"icon": taskIcon
 	},
 	{
 		"id": 10,
 		"studentName": "Delilah",
 		"classCode": "LD-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 57,
 		"preTest": 53,
-		"postTest": 54
+		"postTest": 54,
+		"icon": taskIcon
 	},
 	{
 		"id": 11,
 		"studentName": "Desirae",
 		"classCode": "LC-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 70,
 		"preTest": 58,
-		"postTest": 53
+		"postTest": 53,
+		"icon": taskIcon
 	},
 	{
 		"id": 12,
 		"studentName": "Jasmine",
 		"classCode": "LB-02",
+		"lastUpdate": "21 Okt 2023",
 		"task": 73,
 		"preTest": 88,
-		"postTest": 77
+		"postTest": 77,
+		"icon": taskIcon
 	},
 	{
 		"id": 13,
 		"studentName": "Clinton",
 		"classCode": "LD-02",
+		"lastUpdate": "21 Okt 2023",
 		"task": 99,
 		"preTest": 87,
-		"postTest": 51
+		"postTest": 51,
+		"icon": taskIcon
 	},
 	{
 		"id": 14,
 		"studentName": "Sharon",
 		"classCode": "LC-02",
+		"lastUpdate": "21 Okt 2023",
 		"task": 51,
 		"preTest": 84,
-		"postTest": 58
+		"postTest": 58,
+		"icon": taskIcon
 	},
 	{
 		"id": 15,
 		"studentName": "Castor",
 		"classCode": "LD-02",
+		"lastUpdate": "21 Okt 2023",
 		"task": 78,
 		"preTest": 67,
-		"postTest": 52
+		"postTest": 52,
+		"icon": taskIcon
 	},
 	{
 		"id": 16,
 		"studentName": "Chandler",
 		"classCode": "LB-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 97,
 		"preTest": 92,
-		"postTest": 61
+		"postTest": 61,
+		"icon": taskIcon
 	},
 	{
 		"id": 17,
 		"studentName": "Mira",
 		"classCode": "LD-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 98,
 		"preTest": 50,
-		"postTest": 80
+		"postTest": 80,
+		"icon": taskIcon
 	},
 	{
 		"id": 18,
 		"studentName": "Addison",
 		"classCode": "LC-02",
+		"lastUpdate": "19 Okt 2023",
 		"task": 84,
 		"preTest": 61,
-		"postTest": 90
+		"postTest": 90,
+		"icon": taskIcon
 	},
 	{
 		"id": 19,
 		"studentName": "Martha",
 		"classCode": "LB-02",
+		"lastUpdate": "18 Okt 2023",
 		"task": 87,
 		"preTest": 69,
-		"postTest": 96
+		"postTest": 96,
+		"icon": taskIcon
 	},
 	{
 		"id": 20,
 		"studentName": "Amity",
 		"classCode": "LD-02",
+		"lastUpdate": "20 Okt 2023",
 		"task": 64,
 		"preTest": 99,
-		"postTest": 63
+		"postTest": 63,
+		"icon": taskIcon
 	},
 	{
 		"id": 21,
 		"studentName": "Yen",
 		"classCode": "LC-02",
+		"lastUpdate": "20 Okt 2023",
 		"task": 68,
 		"preTest": 79,
-		"postTest": 97
+		"postTest": 97,
+		"icon": taskIcon
 	},
 	{
 		"id": 22,
 		"studentName": "April",
 		"classCode": "LB-02",
+		"lastUpdate": "20 Okt 2023",
 		"task": 90,
 		"preTest": 93,
-		"postTest": 92
+		"postTest": 92,
+		"icon": taskIcon
 	},
 	{
 		"id": 23,
 		"studentName": "Mia",
 		"classCode": "LB-02",
+		"lastUpdate": "20 Okt 2023",
 		"task": 84,
 		"preTest": 79,
-		"postTest": 67
+		"postTest": 67,
+		"icon": taskIcon
 	},
 	{
 		"id": 24,
 		"studentName": "Anika",
 		"classCode": "LD-02",
+		"lastUpdate": "20 Okt 2023",
 		"task": 89,
 		"preTest": 69,
-		"postTest": 99
+		"postTest": 99,
+		"icon": taskIcon
 	},
 	{
 		"id": 25,
 		"studentName": "Beck",
 		"classCode": "LC-02",
+		"lastUpdate": "20 Okt 2023",
 		"task": 73,
 		"preTest": 75,
-		"postTest": 53
+		"postTest": 53,
+		"icon": taskIcon
 	},
 	{
 		"id": 26,
 		"studentName": "Kibo",
 		"classCode": "LD-02",
+		"lastUpdate": "20 Okt 2023",
 		"task": 83,
 		"preTest": 75,
-		"postTest": 60
+		"postTest": 60,
+		"icon": taskIcon
 	},
 	{
 		"id": 27,
 		"studentName": "Harlan",
 		"classCode": "LB-02",
+		"lastUpdate": "20 Okt 2023",
 		"task": 70,
 		"preTest": 84,
-		"postTest": 75
+		"postTest": 75,
+		"icon": taskIcon
 	},
 	{
 		"id": 28,
 		"studentName": "Elvis",
 		"classCode": "LC-02",
+		"lastUpdate": "22 Okt 2023",
 		"task": 86,
 		"preTest": 87,
-		"postTest": 75
+		"postTest": 75,
+		"icon": taskIcon
 	},
 	{
 		"id": 29,
 		"studentName": "Dana",
 		"classCode": "LB-02",
+		"lastUpdate": "22 Okt 2023",
 		"task": 99,
 		"preTest": 66,
-		"postTest": 84
+		"postTest": 84,
+		"icon": taskIcon
 	},
 	{
 		"id": 30,
 		"studentName": "Francis",
 		"classCode": "LB-02",
+		"lastUpdate": "22 Okt 2023",
 		"task": 71,
 		"preTest": 99,
-		"postTest": 59
+		"postTest": 59,
+		"icon": taskIcon
 	}
 ]
